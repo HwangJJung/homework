87ebbe28a61d139f1f659f30ab7f6cc4ff198028
JJUNG <marvel94@nhnnext.org>
2014-06-01 22:40:27 +0900

    a

---
+++ LinkedListExamlpe/LinkedListExamlpe/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace ListExample {
+
+	class MainClass {
+		class Term{
+			public double coef;
+			public int exp;
+			public Term next;
+			public Term prev;
+		}
+
+		class Polynomial{
+			public Term first;
+			public Polynomial (){
+				first = null;
+			}
+			
+			public void AddTerm(double coef, int exp) {
+				Term t = new Term();
+				t.coef = coef;
+				t.exp = exp;
+				if (first == null) {
+					first = t;
+					t.next = null;
+				} else {
+					t.next = first;
+					first = t;
+				}
+			}
+			public void DeleteTerm(Term t) {
+				t.coef = 0;
+			}
+
+			public void PrintPoly(){
+				for (Term t = first; t != null; t = t.next) {
+					Console.Write ("+");
+					Console.Write("({0} x^{1})", t.coef, t.exp);
+
+				}
+				Console.WriteLine ();
+			}
+			public Polynomial Simplify() {
+				Polynomial res = new Polynomial ();
+				for ( Term t = first; t != null; t=t.next){
+			
+					int exp= t.exp;
+					double coef = t.coef;
+
+					for (Term s = t.next; s != null; s=s.next) {
+						if (s.exp == exp) {
+							coef += s.coef;
+							DeleteTerm (s);
+						}
+					}
+					res.AddTerm (coef, exp);
+				}
+				return res;
+			}
+		static void Main() {
+				Polynomial p = new Polynomial ();
+				p.AddTerm (1, 2);
+				p.AddTerm (-1, 1);
+				p.AddTerm (-1, 1);
+				p.AddTerm (1, 0);
+
+				p.PrintPoly ();
+				Polynomial s = p.Simplify ();
+				s.PrintPoly ();
+			}
+		}
+	}
+}
---
+++ 자구알/ramanujan.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{       
+	class MainClass
+	{
+		//세제곱 시키는 함수 
+		
+		private static int Cube(int i)
+		{
+			i = i*i*i;
+			return i;
+		}
+
+		public static void Main (string[] args)
+		{
+			int[] cube = new int[100];
+			List<int> resultList = new List<int> ();
+
+			for (int i =1; i<=100; i++){
+				cube.SetValue (Cube(i), i-1);
+			}
+			for ( int i=0; i<100; i++ ) {
+				for ( int j = i+1; j < 100; j++) {
+					resultList.Add (cube[i] + cube[j]);
+				}
+			}
+			resultList.Sort ();
+			for ( int i=0; i < 500; i++) {
+				if (resultList[i] == resultList[i+1]) {
+					Console.WriteLine( resultList[i]);
+				}
+			}
+            Console.ReadLine();
+		}
+		
+	}
+}
